9e5d65c229ad0a72fc0679b3d983130c8a9c4a10
Jeremiah Chienda <jeremiahchienda@gmail.com>
2024-03-31 12:41:43 +0200

    added wa link

--- src/components/AppStoreLink.jsx
+++ src/components/AppStoreLink.jsx
@@ -1,22 +1,37 @@
 import Link from 'next/link'
 import clsx from 'clsx'
-import Image from "next/image";
+import Image from 'next/image'
 
-export function AppStoreLink({color = 'black', alt = false, link = "#"}) {
-    const src = alt ? '/images/beta-ios.png' : '/images/beta-android.png'
-    return (
-        <Link
-            href={link}
-            target={"_blank"}
-            aria-label="Pre-order on the App Store"
-            className={clsx(
-                'rounded-lg transition-colors',
-                color === 'black'
-                    ? 'bg-gray-800 text-white hover:bg-gray-900'
-                    : 'bg-white text-gray-900 hover:bg-gray-50'
-            )}
-        >
-            <Image src={src} alt="Pre-order Now" className={"rounded-lg border-2 border-slate-400"} width={150} height={44.04} />
-        </Link>
-    )
+export function AppStoreLink({
+  color = 'black',
+  alt = false,
+  link = '#',
+  whatsapp = false,
+}) {
+  const src = whatsapp
+    ? '/images/whatsapp-order.png'
+    : alt
+    ? '/images/beta-ios.png'
+    : '/images/beta-android.png'
+  return (
+    <Link
+      href={link}
+      target={'_blank'}
+      aria-label="Pre-order on the App Store"
+      className={clsx(
+        'rounded-lg transition-colors',
+        color === 'black'
+          ? 'bg-gray-800 text-white hover:bg-gray-900'
+          : 'bg-white text-gray-900 hover:bg-gray-50'
+      )}
+    >
+      <Image
+        src={src}
+        alt="Pre-order Now"
+        className={'rounded-lg border-2 border-slate-400'}
+        width={150}
+        height={44.04}
+      />
+    </Link>
+  )
 }
--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -1,27 +1,42 @@
-import React from 'react';
-import {Container} from "@/components/Container";
-import Image from "next/image";
-import Title from "@/components/Title";
-import {AppStoreLink} from "@/components/AppStoreLink";
+import React from 'react'
+import { Container } from '@/components/Container'
+import Image from 'next/image'
+import Title from '@/components/Title'
+import { AppStoreLink } from '@/components/AppStoreLink'
 
 function Download() {
-    return (
-        <section className="py-20 bg-accent" id={"download"}>
-            <Container className="flex flex-col lg:flex-row">
-                <div className="w-full lg:w-1/2">
-                    <Image src="/images/screenshots.png" alt="App Screenshots" width={1080} height={720} />
-                </div>
-                <div className="w-full my-12 lg:my-4 lg:w-1/2 flex flex-col justify-center text-center">
-                    <Title title="Download the app" />
-                    <p>Order anything and have it delivered to your doorstep today</p>
-                    <div className="my-8 flex justify-center gap-x-4">
-                        <AppStoreLink link={"https://testflight.apple.com/join/UyPQsCc6"} alt />
-                        <AppStoreLink link={"https://play.google.com/apps/testing/com.doorbell.mw.app"} />
-                    </div>
-                </div>
-            </Container>
-        </section>
-    );
+  return (
+    <section className="bg-accent py-20" id={'download'}>
+      <Container className="flex flex-col lg:flex-row">
+        <div className="w-full lg:w-1/2">
+          <Image
+            src="/images/screenshots.png"
+            alt="App Screenshots"
+            width={1080}
+            height={720}
+          />
+        </div>
+        <div className="my-12 flex w-full flex-col justify-center text-center lg:my-4 lg:w-1/2">
+          <Title title="Download the app" />
+          <p>
+            Sorry, our Beta is currently oversubscribed. But you can order
+            anything and have it delivered to your through our App, starting{' '}
+            <strong>April 21</strong>
+          </p>
+          <div className="my-8 flex justify-center gap-x-4">
+            {/* <AppStoreLink link={"https://testflight.apple.com/join/UyPQsCc6"} alt />
+                        <AppStoreLink link={"https://play.google.com/apps/testing/com.doorbell.mw.app"} /> */}
+            <AppStoreLink
+              whatsapp
+              link={
+                'https://wa.me/265999889000?text=I%20want%20to%20join%20the%20Doorbell%20App%20Beta%20Program'
+              }
+            />
+          </div>
+        </div>
+      </Container>
+    </section>
+  )
 }
 
-export default Download;
+export default Download
