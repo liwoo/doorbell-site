77cb363b2f38524abf59d2688c10d9eeb232ff25
Jeremiah Chienda <jeremiahchienda@gmail.com>
2024-08-16 23:20:03 +0200

    fixed crisp script issue

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -4,10 +4,25 @@ export default function Document() {
   return (
     <Html className="h-full bg-gray-50 antialiased" lang="en">
       <Head />
+      <script
+        type="text/javascript"
+        dangerouslySetInnerHTML={{
+          __html: `
+                     window.$crisp=[];
+                     window.CRISP_WEBSITE_ID="847cf360-b72b-43ba-a971-57346a35e2ac";
+                     (function(){
+                       d=document;
+                       s=d.createElement("script");
+                       s.src="https://client.crisp.chat/l.js";
+                       s.async=1;
+                       d.getElementsByTagName("head")[0].appendChild(s);
+                     })();
+                   `,
+        }}
+      />
       <body className="flex h-full flex-col">
         <Main />
         <NextScript />
-        <script type="text/javascript">window.$crisp=[];window.CRISP_WEBSITE_ID="847cf360-b72b-43ba-a971-57346a35e2ac";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();</script>
       </body>
     </Html>
   )
