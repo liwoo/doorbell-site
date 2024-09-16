40160d1e1485fb0173e9b278fc7602d5f78ee499
Daniel P. Mgawi <pdmgawi@gmail.com>
2024-09-16 09:37:17 +0200

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,24 +1,23 @@
 import Head from 'next/head'
-
-import {CallToAction} from '@/components/CallToAction'
-import {Hero} from '@/components/Hero'
-import {Reviews} from '@/components/Reviews'
-import {SecondaryFeatures} from '@/components/SecondaryFeatures'
-import Contact from "@/components/Contact";
-import Testimonies from "@/components/Testimonies";
-import Download from "@/components/Download";
-import MainLayout from "@/layouts/MainLayout";
+import { CallToAction } from '@/components/CallToAction'
+import { Hero } from '@/components/Hero'
+import { Reviews } from '@/components/Reviews'
+import { SecondaryFeatures } from '@/components/SecondaryFeatures'
+import Contact from '@/components/Contact'
+import Testimonies from '@/components/Testimonies'
+import Download from '@/components/Download'
+import MainLayout from '@/layouts/MainLayout'
 
 export default function Home() {
-    return (
-        <MainLayout>
-            <Hero/>
-            <SecondaryFeatures/>
-            <CallToAction/>
-            <Reviews/>
-            <Contact/>
-            <Testimonies />
-            <Download />
-        </MainLayout>
-    )
+  return (
+    <MainLayout>
+      <Hero />
+      <SecondaryFeatures />
+      <CallToAction />
+      <Reviews />
+      <Contact />
+      <Testimonies />
+      <Download />
+    </MainLayout>
+  )
 }
