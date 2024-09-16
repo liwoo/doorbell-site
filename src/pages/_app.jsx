e1c663edfcc11a7e8d85970b06eb56656ed9407c
Daniel P. Mgawi <pdmgawi@gmail.com>
2024-09-16 09:36:21 +0200

    Update _app.jsx

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,6 +1,22 @@
 import '@/styles/tailwind.css'
 import 'focus-visible'
+import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }) {
+  return (
+    <GoogleReCaptchaProvider
+      reCaptchaKey={process.env.PUBLIC_RECAPTCHA_KEY}
+      scriptProps={{
+        async: true,
+        defer: false,
+        useEnterprise: true,
+        appendTo: 'head',
+        nonce: undefined,
+      }}
+    >
+      <Component {...pageProps} />
+    </GoogleReCaptchaProvider>
+  )
 }
+
+export default App
