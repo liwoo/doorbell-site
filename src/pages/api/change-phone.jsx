3367bf4796fff1948921c91f06b450e9cff359a1
Daniel P. Mgawi <pdmgawi@gmail.com>
2024-10-16 14:49:26 +0200

    Create change-phone.jsx

---
+++ src/pages/api/change-phone.jsx
@@ -0,0 +1,31 @@
+import sgMail from '@sendgrid/mail'
+
+sgMail.setApiKey(process.env.SENDGRID_KEY)
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default async (req, res) => {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ message: 'Not Allowed' })
+  }
+
+  const data = req.body.data
+
+  const { phone, customer_number } = data
+  try {
+    const html = `Phone: ${phone} <br> Customer Number: ${customer_number}`
+
+    const msg = {
+      to: process.env.TO_EMAIL,
+      from: process.env.VERIFIED_SENDGRID_SENDER,
+      subject: 'Change My Doorbell Account Phone Number',
+      text: `Phone: ${phone}\nCustomer Number: ${customer_number}`,
+      html: html,
+    }
+
+    await sgMail.send(msg)
+
+    res.json({ message: 'Email has been sent to our inbox!' })
+  } catch (error) {
+    res.status(500).json({ error: 'Error sending email' })
+  }
+}
