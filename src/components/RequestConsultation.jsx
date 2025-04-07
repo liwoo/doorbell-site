b8d1e388a7f12bfcf94843ae05df4690f2a260dc
Daniel P. Mgawi <pdmgawi@gmail.com>
2025-04-07 15:57:08 +0200

    Create RequestConsultation.jsx

---
+++ src/components/RequestConsultation.jsx
@@ -0,0 +1,30 @@
+export default function RequestConsultation() {
+  return (
+    <section className="bg-primary-dark" id={'get-in-touch'}>
+      <div className="px-6 py-24 sm:px-6 sm:py-32 lg:px-8">
+        <div className="mx-auto max-w-2xl text-center">
+          <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
+            START OPTIMIZING YOUR BUSINESS OPERATIONS TODAY
+          </h2>
+          <p className="mx-auto mt-6 max-w-xl text-lg leading-8 text-indigo-200">
+            Schedule a consultation with our business solutions team to design a
+            custom implementation plan for your organization. Discover how
+            DoorBell for Business can transform your operational efficiency
+            within days, not months.
+          </p>
+          <div className="mt-10 flex items-center justify-center gap-x-6">
+            <a
+              href="mailto:cto@doorbell.mw"
+              className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-indigo-600 shadow-sm hover:bg-indigo-50 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+            >
+              Request A Consultation
+            </a>
+          </div>
+          <p className="mx-auto mt-6 max-w-xl text-lg leading-8 text-indigo-200">
+            Contact: info@doorbell.mw | +265 99 999 99 99
+          </p>
+        </div>
+      </div>
+    </section>
+  )
+}
